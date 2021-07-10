210e9ad8c578c9eb7e23a3992c31a1824e4f8dfa
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2021-07-10 18:23:24 -0300

    Fix component name of bottom button

--- src/components/BottomButton/index.tsx
+++ src/components/BottomButton/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { TouchableOpacityProps } from 'react-native';
 
 import { Container, ButtonText } from './styles';
 
-interface HomeProps {
+interface HomeProps extends TouchableOpacityProps {
   text: string;
   light?: boolean;
 }
 
-export default function Home({ text, light = false }: HomeProps) {
+export default function BottomButton({
+  text,
+  light = false,
+  ...rest
+}: HomeProps) {
   return (
-    <Container light={light}>
+    <Container light={light} {...rest}>
       <ButtonText light={light}>{text}</ButtonText>
     </Container>
   );
