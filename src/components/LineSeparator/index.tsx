2ad4ade10a71fc697fb7cf1c2e2ef0b23294335d
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2021-07-10 18:59:57 -0300

    Add line component

---
+++ src/components/LineSeparator/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import Line from '../../assets/Doc/Line.png';
+
+export default function LineSeparator() {
+  return <Container source={Line} />;
+}
