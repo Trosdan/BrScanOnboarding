70e767c7492cdd6fcd5543c6158c455f87ddb034
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2021-07-10 22:50:15 -0300

    Add close icon on component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,24 +3,39 @@ import React from 'react';
 import IconLeft from '../../assets/IconLeft.png';
 import IconLeftDark from '../../assets/IconLeftDark.png';
 
-import { Container, ButtonImageLeft, ImageLeft } from './styles';
+import IconClose from '../../assets/Doc/IconClose.png';
+import IconCloseDark from '../../assets/Doc/IconCloseDark.png';
+
+import { Container, ButtonImageLeft, Text, ImageLeft } from './styles';
 
 interface HeaderProps {
   light?: boolean;
   onBackPress?: () => void;
   showBackButton?: boolean;
+  text?: string;
+  onClosePress?: () => void;
+  showCloseButton?: boolean;
 }
 
 export default function Header({
   light = false,
   onBackPress,
   showBackButton = true,
+  text = '',
+  onClosePress,
+  showCloseButton = false,
 }: HeaderProps) {
   return (
     <Container>
       {showBackButton && (
         <ButtonImageLeft onPress={onBackPress}>
           <ImageLeft source={light ? IconLeft : IconLeftDark} />
+        </ButtonImageLeft>
+      )}
+      <Text light={light}>{text}</Text>
+      {showCloseButton && (
+        <ButtonImageLeft onPress={onClosePress}>
+          <ImageLeft source={light ? IconClose : IconCloseDark} />
         </ButtonImageLeft>
       )}
     </Container>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components/native';
 
+interface LightMode {
+  light: boolean;
+}
+
 export const Container = styled.View`
   height: 55px;
   flex-direction: row;
@@ -9,6 +13,15 @@ export const Container = styled.View`
 
 export const ButtonImageLeft = styled.TouchableOpacity`
   margin: 0 8px;
+`;
+
+export const Text = styled.Text<LightMode>`
+  flex: 1;
+
+  text-align: right;
+  font-family: ${({ theme }) => theme.fonts.IBMPlexSans};
+  color: ${({ theme, light }) =>
+    light ? theme.colors.textWhite : theme.colors.textBlack};
 `;
 
 export const ImageLeft = styled.Image``;
