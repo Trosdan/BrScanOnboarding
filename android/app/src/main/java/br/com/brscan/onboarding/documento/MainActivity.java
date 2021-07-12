d67f74948b7051aefdd05acc05ef32f73b6496cb
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2021-07-11 21:17:50 -0300

    Change bundle id

--- android/app/src/main/java/br/com/brscan/onboarding/documento/MainActivity.java
+++ android/app/src/main/java/br/com/brscan/onboarding/documento/MainActivity.java
@@ -1,4 +1,4 @@
-package com.brscanonboarding;
+package br.com.brscan.onboarding.documento;
 
 import android.content.Intent;
 import android.content.res.Configuration;
--- android/app/src/main/java/br/com/brscan/onboarding/documento/MainApplication.java
+++ android/app/src/main/java/br/com/brscan/onboarding/documento/MainApplication.java
@@ -1,4 +1,4 @@
-package com.brscanonboarding;
+package br.com.brscan.onboarding.documento;
 
 import android.app.Application;
 
